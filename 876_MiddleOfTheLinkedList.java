d1fdb121bd0ab75273c9006f7eec2381cc0e44d9
Danda Keerthi <dandakeerthi2@gmail.com>
2026-01-01 11:57:16 +0530

    Create 876_MiddleOfTheLinkedList.java

---
+++ 876_MiddleOfTheLinkedList.java
@@ -0,0 +1,16 @@
+// LeetCode 876 - Middle of the Linked List
+// Approach: Slow & Fast Pointer
+// Time: O(n), Space: O(1)
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
