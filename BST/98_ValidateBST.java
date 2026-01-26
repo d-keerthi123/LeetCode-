29ae35c2e63a970a552e7245a8623af827e0a260
Danda Keerthi <dandakeerthi2@gmail.com>
2026-01-26 11:15:37 +0530

    Create 98_ValidateBST.java

---
+++ BST/98_ValidateBST.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValidBST(TreeNode root){
+        return helper(root,null,null);
+    }
+    public boolean helper(TreeNode root,TreeNode min,TreeNode max) {
+        if(root == null){
+            return true;
+        }
+        if(min != null && root.val<=min.val){ // it should be : root > min
+            return false;
+        }
+        else if(max != null && root.val >= max.val){ // it should be : root < max
+            return false;
+        }
+
+        return helper(root.left,min,root) && helper(root.right,root,max);
+    }
+}
