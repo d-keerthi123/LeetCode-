42378963e884b47190829c09cbc328e052ecd6e8
Danda Keerthi <dandakeerthi2@gmail.com>
2026-02-06 20:59:13 +0530

    Create 1_TwoSum.java

---
+++ Arrays/1_TwoSum.java
@@ -0,0 +1,17 @@
+//Time Complexity O(n^2)
+//Space Complexity O(1)
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+      
+      //Brute Force Approach
+        for(int i=0;i<nums.length ;i++){
+            for(int j=i+1 ;j<nums.length ;j++){
+                if(nums[i] +nums[j] == target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
